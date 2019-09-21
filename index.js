80d701127778b9db69eee2c8ab96bee31fa1fa93
Alper Ozisik <alper.ozisik@yahoo.com>
2019-09-22 00:11:33 +0300

    changes

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
 console.log("hello world");
-require("./b.js");
---
+++ test/test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../');
+
+chai.use(chaiHttp);
+chai.should();
+describe('Hello World', () => {
+    describe('GET /', () => {
+        it('should get "Hello World!" response', (done) => {
+            chai.request(app).
+                get('/').
+                end((err, res) => {
+                    console.error(err);
+                    res.should.have.status(200);
+                    res.text.should.be.a('string');
+                    res.text.should.be.equal('Hello World!')
+                    done();
+                });
+        });
+    });
+});
